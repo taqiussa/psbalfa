1564f3f2bff47fdab8c9747f0852cab6f10c8317
taqiussa <taqiussa@gmail.com>
2022-09-08 07:06:46 +0700

    Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,14 +14,6 @@
 
     <!-- Styles -->
     <style>
-        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
-
-        html {
-            font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-        }
-    </style>
-
-    <style>
         body {
             font-family: 'Nunito', sans-serif;
         }
@@ -30,37 +22,37 @@
 
 </head>
 
-<body class="leading-normal tracking-normal text-indigo-400 m-6 bg-gradient-to-t from-slate-100 to-green-600">
-    <div class="h-screen">
+<body class="m-6 leading-normal tracking-normal text-indigo-400 bg-gradient-to-t from-slate-100 to-green-600">
+    <div class="min-h-screen ">
         <!--Nav-->
-        <div class="w-full container mx-auto">
-            <div class="w-full flex items-center justify-between">
+        <div class="container w-full mx-auto">
+            <div class="flex items-center justify-between w-full">
                 <span
-                    class="flex items-center font-bold text-2xl lg:text-4xl text-white">PSB | SMP AL MUSYAFFA</span>
+                    class="flex items-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-slate-100 to-teal-400 lg:text-4xl">PSB | SMP AL MUSYAFFA</span>
 
-                <div class="flex w-1/2 justify-end content-center">
-                    <img src="{{ asset('images/logoalfa2.png') }}" alt="logo" class="w-24 hover:scale-105 duration-700">
+                <div class="flex content-center justify-end w-1/2">
+                    <img src="{{ asset('images/logoalfa2.png') }}" alt="logo" class="w-24 duration-700 hover:scale-105">
                 </div>
             </div>
         </div>
 
         <!--Main-->
-        <div class="container pt-4 md:pt-4 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <div class="container flex flex-col flex-wrap items-center pt-4 mx-auto md:pt-4 md:flex-row">
             <!--Left Col-->
-            <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+            <div class="flex flex-col justify-center w-full overflow-y-hidden xl:w-2/5 lg:items-start">
                 <h1
-                    class="my-2 text-2xl md:text-4xl text-teal-600 opacity-75 font-bold leading-tight text-center md:text-left">PORTAL
+                    class="my-2 text-2xl font-bold leading-tight text-center text-teal-600 opacity-75 md:text-4xl md:text-left">PORTAL
                     PENDAFTARAN SANTRI BARU!
                 </h1>
-                <p class="leading-normal text-teal-600 md:text-2xl mb-8 text-center md:text-left">
+                <p class="mb-8 leading-normal text-center text-teal-600 md:text-2xl md:text-left">
                     Pendaftaran Belum dibuka, Pantau Terus😊
                 </p>
 
             </div>
 
             <!--Right Col-->
-            <div class="w-full xl:w-3/5 p-12 overflow-hidden">
-                <img class="mx-auto w-full md:w-4/5 transform  transition hover:scale-105 duration-700 ease-in-out hover:rotate-6"
+            <div class="w-full p-12 overflow-hidden xl:w-3/5">
+                <img class="w-full mx-auto transition duration-700 ease-in-out transform md:w-4/5 hover:scale-105 hover:rotate-6"
                     src="{{ asset('images/educator.svg') }}" />
             </div>
 
